ae8ba051846b01fb78ac4043c32f88bb17eb8ef2
Umit Dincel <dincelumit@gmail.com>
2020-05-31 18:39:43 +0300

    added a function to get elapsed time

--- helpers.c
+++ helpers.c
@@ -32,3 +32,16 @@ floatTYPE** createFloatSquareMatrix(int dimension, float value){
 
     return matrix;
 }
+
+
+double getElapsedTime(struct timeval t1, struct timeval t0){
+    return (t1.tv_sec-t0.tv_sec) * 1.0f + (t1.tv_usec - t0.tv_usec) / 1000000.0f;
+}
+
+double getMFlops(int iterations, double elapsedTime){
+    double Flops = iterations/elapsedTime;
+    // Calculate Mega Flops Formula: F lops * 10raised to (-6).
+    double mFlops = (double)Flops/1000000;
+
+    return mFlops;
+}
